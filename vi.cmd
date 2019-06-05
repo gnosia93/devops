0de2da9068d972baa1cbdf89908534c8ef67af0e
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2019-06-05 15:41:00 +0900

    Create vi.cmd

---
+++ vi.cmd
@@ -0,0 +1,19 @@
+## .VIMRC 파일에 설정하기 ##
+
+$ cd
+
+$ vi .vimrc
+
+```
+syntax on
+
+set nu
+set tabstop=4
+set autoindent
+set ruler
+set showcmd
+set title
+set wmnu
+set showmatch
+set nocompatible
+```
